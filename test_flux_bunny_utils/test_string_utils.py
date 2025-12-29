054728db7791a06a2848567ffd729cc555088cc3
rebecca <rebeccarashkin@gmail.com>
2025-12-28 17:25:06 -0700

    Added string_utils unit tests

--- test_flux_bunny_utils/test_string_utils.py
+++ test_flux_bunny_utils/test_string_utils.py
@@ -25,3 +25,75 @@
 #   DESCRIPTION
 #   Tests for string utility functions.
 #______________________________________________________________________
+
+import pytest
+
+from flux_bunny_utils.string_utils import StringUtils
+#_______________________________________________________________________
+class TestConst:
+  """
+  Contains constants used in tests.
+  """
+
+  ALL_CAPS_2_CHAR_WITH_0X: str = '0XFF'
+  MIX_CAPS_2_CHAR_WITH_0X: str = '0xFf'
+  ALL_CAPS_6_CHAR_WITH_0X: str = '0XFFFFFF'
+  MIX_CAPS_6_CHAR_WITH_0X: str = '0xFfFFff'
+
+  ALL_CAPS_2_CHAR_NO_PRFX: str = 'FF'
+  MIX_CAPS_2_CHAR_NO_PRFX: str = 'fF'
+  ALL_CAPS_6_CHAR_NO_PRFX: str = 'FFFFFF'
+  MIX_CAPS_6_CHAR_NO_PRFX: str = 'fFfFFf'
+
+  ERR_VAL_WITH_0X: str = '0x5g'
+
+  INT_2_CHAR: int = 255
+  INT_6_CHAR: int = 16777215
+
+  ERR_VAL: int = -1
+
+#_______________________________________________________________________
+def test_str_hex2int_all_caps_with_0x():
+  assert(
+    StringUtils.str_hex_to_int(TestConst.ALL_CAPS_2_CHAR_WITH_0X) ==\
+    TestConst.INT_2_CHAR
+  )
+  assert(
+    StringUtils.str_hex_to_int(TestConst.ALL_CAPS_6_CHAR_WITH_0X) ==\
+    TestConst.INT_6_CHAR
+  )
+
+def test_str_hex2int_mix_caps_with_0x():
+  assert(
+    StringUtils.str_hex_to_int(TestConst.MIX_CAPS_2_CHAR_WITH_0X) ==\
+    TestConst.INT_2_CHAR
+  )
+  assert(
+    StringUtils.str_hex_to_int(TestConst.MIX_CAPS_6_CHAR_WITH_0X) ==\
+    TestConst.INT_6_CHAR
+  )
+
+def test_str_hex2int_all_caps_no_prfx():
+  assert(
+    StringUtils.str_hex_to_int(TestConst.ALL_CAPS_2_CHAR_NO_PRFX) ==\
+    TestConst.INT_2_CHAR
+  )
+  assert(
+    StringUtils.str_hex_to_int(TestConst.ALL_CAPS_6_CHAR_NO_PRFX) ==\
+    TestConst.INT_6_CHAR
+  )
+
+def test_str_hex2int_mix_caps_no_prfx():
+  assert(
+    StringUtils.str_hex_to_int(TestConst.MIX_CAPS_2_CHAR_NO_PRFX) ==\
+    TestConst.INT_2_CHAR
+  )
+  assert(
+    StringUtils.str_hex_to_int(TestConst.MIX_CAPS_6_CHAR_NO_PRFX) ==\
+    TestConst.INT_6_CHAR
+  )
+
+#_______________________________________________________________________
+def test_str_hex2int_err():
+  with pytest.raises(Exception):
+    StringUtils.str_hex_to_int(TestConst.ERR_VAL_WITH_0X)
